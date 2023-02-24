a4503acb7d621f89f4408922f42f966bb59e32df
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-02-24 16:37:23 +0530

    redis init

---
+++ src/config/redisSetup.ts
@@ -0,0 +1,21 @@
+import { createClient } from "redis";
+
+// const redisClient = createClient({
+//     password: 'h0yBrrOOCpZa7FqyYVglGhrpN00oeu8z',
+//     socket: {
+//         host: 'redis-19278.c57.us-east-1-4.ec2.cloud.redislabs.com',
+//         port: 19278,
+//     },
+// });
+
+const redisClient = createClient();
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
+
+export async function connectRedis() {
+    await redisClient.connect();
+}
+
+export default redisClient;
+
+// userId:Source:Type
