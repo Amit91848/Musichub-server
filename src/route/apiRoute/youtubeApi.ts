aa1f08e996e075767ea42d2058aa4d092cfadeff
Amit Sharma <amitsharma@Amits-MacBook-Air.local>
2023-02-24 17:25:07 +0530

    youtube route caching

--- src/route/apiRoute/youtubeApi.ts
+++ src/route/apiRoute/youtubeApi.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { getYoutubePlaylistsItems, getYoutubeSearchQuery } from '../../controllers/ApiController/youtube'
 import { mapYoutubePlaylistItemsToCommonTracks, mapYoutubeQueryResultToCommonTracks } from "../../utils/map";
+import redisClient from "../../config/redisSetup";
 
 const youtubeApi = Router();
 
@@ -8,19 +9,34 @@ youtubeApi.get('/playlist/:playlistId/tracks', async (req, res) => {
     const playlistId = req.params.playlistId;
     const userId = req.user.userId;
 
-    const playlistItems = await getYoutubePlaylistsItems(playlistId, userId);
-    const mappedTracks = mapYoutubePlaylistItemsToCommonTracks(playlistItems);
+    const cachedPlaylist = await redisClient.get(`youtube:playlist:${playlistId}`);
 
-    res.status(200).json(mappedTracks);
+    if (cachedPlaylist) {
+        return res.status(200).json(JSON.parse(cachedPlaylist));
+    } else {
+        const playlistItems = await getYoutubePlaylistsItems(playlistId, userId);
+        const mappedTracks = mapYoutubePlaylistItemsToCommonTracks(playlistItems);
+
+        await redisClient.set(`youtube:playlist:${playlistId}`, JSON.stringify(mappedTracks));
+
+        return res.status(200).json(mappedTracks);
+    }
 })
 
 youtubeApi.get('/search/:query', async (req, res) => {
     const query = req.params.query;
     const userId = req.user.userId;
 
-    const queryResults = await getYoutubeSearchQuery(query, userId);
-    const mappedQuery = mapYoutubeQueryResultToCommonTracks(queryResults.items);
-    res.status(200).json(mappedQuery);
+    const cachedQuery = await redisClient.get(`youtube:search:${query}`);
+
+    if (cachedQuery) {
+        return res.status(200).json(JSON.parse(cachedQuery));
+    } else {
+        const queryResults = await getYoutubeSearchQuery(query, userId);
+        const mappedQuery = mapYoutubeQueryResultToCommonTracks(queryResults.items);
+        await redisClient.set(`youtube:search:${query}`, JSON.stringify(mappedQuery));
+        return res.status(200).json(mappedQuery);
+    }
 })
 
 youtubeApi.get('/', (_, res) => {
